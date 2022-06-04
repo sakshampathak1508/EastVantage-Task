be1238b04eeb39687321ae2c8719b8ff494047ba
saksham <sakshamvpathak@gmail.com>
2022-06-04 23:40:02 +0530

    another commit

--- eastvantage/settings.py
+++ eastvantage/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
